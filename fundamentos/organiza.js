71e4e3b23e42fda0fd59bf30b9b2641ffe3b39ab
Paulo Palmeira <paulopalmeirasp@gmail.com>
2023-06-24 17:27:44 -0300

    começando

---
+++ fundamentos/organiza.js
@@ -0,0 +1,6 @@
+a = 3
+b = 9
+
+let qualquer="Legal"
+
+console.log(typeof qualquer)
---
+++ stringona.js
@@ -0,0 +1,7 @@
+const nome = "Maria"
+const salvador = "Jesus"
+console.log(nome.charAt(3))
+
+console.log("Ave " + nome + ", mãe de " + salvador + " !")
+
+console.log ('Ana, Maria, Pedro' .split(','))
