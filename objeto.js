c3980e6b07b189ac7a2f6625faad65ac6e74b9ee
Paulo Palmeira <paulopalmeirasp@gmail.com>
2023-06-25 20:02:46 -0300

    funcao e objeto

---
+++ objeto.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = 'Geladeira Panasonic'
+prod1.preco = 4998.90
+
+prod1['Desconto'] = 0.40 //evitar atributos com espaço
+
+console.log(prod1)
+
+const prod2 = {
+    nome : 'Camisa',
+    preco : 79.90
+}
+
+console.log(prod2)
+
